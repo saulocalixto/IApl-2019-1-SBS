32bfbf762f71217290a55fb9fc2f7207ee4477bb
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2019-04-07 11:16:33 -0300

    Scripts SQL iniciais

---
+++ SQL/cargaTestes.sql
@@ -0,0 +1,7 @@
+USE bancolegal;
+
+Insert into PESSOA (ID, NOME, CPF, DATANASCIMENTO, ENDERECO) Values (1, 'Saulo Calixto', '28433978047', '1990-09-17', 'Rua Vitoria Régia n 30');
+Insert into PESSOA (ID, NOME, CPF, DATANASCIMENTO, ENDERECO) Values (2, 'Cezar Calixto', '14723602062', '1990-09-18', 'Rua Vitoria Régia n 31');
+Insert into PESSOA (ID, NOME, CPF, DATANASCIMENTO, ENDERECO) Values (3, 'Gessika Mikaele', '90400606062', '1990-09-19', 'Rua Vitoria Régia n 32');
+Insert into PESSOA (ID, NOME, CPF, DATANASCIMENTO, ENDERECO) Values (4, 'Amanda Pereira', '61606806092', '1990-09-20', 'Rua Vitoria Régia n 33');
+Insert into PESSOA (ID, NOME, CPF, DATANASCIMENTO, ENDERECO) Values (5, 'Valkiria Pereira', '35429995045', '1990-09-21', 'Rua Vitoria Régia n 34');
